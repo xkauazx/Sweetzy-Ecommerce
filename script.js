3bfa42cc7455055064e5b98b462693a702210611
xkauazx <kauafernandesmelo@gmail.com>
2025-08-05 18:24:27 -0300

    att

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ buttonCart.forEach((button) => {
         const product = button.closest('.product')
         const container = product.querySelector('.container')
         container.classList.toggle('toggle')
+        button.style.display = 'none'
+
         })
     })
-        console.log('click')
-    
-)
+
+const  btnMore = document.querySelectorAll(".btn-more")    
+const btnLess = document.querySelectorAll(".btn-less")
+let quantity = document. querySelectorAll(".quantitity")
+
+quantity = 0
+
+
+btnLess.forEach((button) =>{
+button.addEventListener('click', () => {
+
+quantity.innerHTML = `${quantity}`
+
+quantity++
+
+})
+})
